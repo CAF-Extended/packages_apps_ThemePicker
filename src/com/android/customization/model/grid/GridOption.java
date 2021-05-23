AOSPA/android_packages_apps_ThemePicker

Fix preview tile in grid option picker.
 * Create GridTileDrawable with cols and rows parameters
   in correct order and display also non quadratic
   configurations correctly.

Change-Id: I6bb64564e86ec002831dc04bf95f51318dc9e6b3

src/com/android/customization/model/grid/GridOption.java
@@ -44,7 +44,7 @@ public class GridOption implements CustomizationOption<GridOption> {
             Uri previewImageUri, int previewPagesCount, String iconShapePath) {
         mTitle = title;
         mIsCurrent = isCurrent;
-        mTileDrawable = new GridTileDrawable(rows, cols, iconShapePath);
+        mTileDrawable = new GridTileDrawable(cols, rows, iconShapePath);
         this.name = name;
         this.rows = rows;
         this.cols = cols;

CAF-Extended/packages_apps_ThemePicker

src/com/android/customization/model/grid/GridOption.java
@@ -62,7 +62,7 @@ public class GridOption implements CustomizationOption<GridOption>, Parcelable {
         mTitle = title;
         mIsCurrent = isCurrent;
         mIconShapePath = iconShapePath;
-        mTileDrawable = new GridTileDrawable(rows, cols, mIconShapePath);
+        mTileDrawable = new GridTileDrawable(cols, rows, mIconShapePath);
         this.name = name;
         this.rows = rows;
         this.cols = cols;