ZVNexus/android_packages_apps_ThemePicker

Limit number of icon previews shown.
Change-Id: I8a1506fd0e8cddbaf4d6df28b75045c2e7801a81

src/com/android/customization/model/theme/custom/ShapeOptionsProvider.java
@@ -53,6 +53,8 @@ import java.util.List;
 public class ShapeOptionsProvider extends ThemeComponentOptionProvider<ShapeOption> {
 
     private static final String TAG = "ShapeOptionsProvider";
+    private static final int MAX_ICON_SHAPE_PREVIEWS = 6;
+
     private final String[] mShapePreviewIconPackages;
     private int mThumbSize;
 
@@ -105,6 +107,10 @@ public class ShapeOptionsProvider extends ThemeComponentOptionProvider<ShapeOpti
     private List<Drawable> getShapedIcons(Path path) {
         List<Drawable> icons = new ArrayList<>();
         for (String packageName : mShapePreviewIconPackages) {
+            if (icons.size() == MAX_ICON_SHAPE_PREVIEWS) {
+                break;
+            }
+
             try {
                 Drawable appIcon = mContext.getPackageManager().getApplicationIcon(packageName);
                 if (appIcon instanceof AdaptiveIconDrawable) {

CAF-Extended/packages_apps_ThemePicker

src/com/android/customization/model/theme/custom/ShapeOptionsProvider.java
@@ -55,6 +55,8 @@ import java.util.List;
 public class ShapeOptionsProvider extends ThemeComponentOptionProvider<ShapeOption> {
 
     private static final String TAG = "ShapeOptionsProvider";
+    private static final int MAX_ICON_SHAPE_PREVIEWS = 6;
+
     private final String[] mShapePreviewIconPackages;
     private int mThumbSize;
 
@@ -108,6 +110,10 @@ public class ShapeOptionsProvider extends ThemeComponentOptionProvider<ShapeOpti
         for (String packageName : mShapePreviewIconPackages) {
             Drawable icon = null;
             CharSequence name = null;
+            if (shapedAppIcons.size() == MAX_ICON_SHAPE_PREVIEWS) {
+                break;
+            }
+
             try {
                 Drawable appIcon = mContext.getPackageManager().getApplicationIcon(packageName);
                 if (appIcon instanceof AdaptiveIconDrawable) {